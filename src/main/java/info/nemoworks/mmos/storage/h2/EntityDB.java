0cf14bc0ed1eb20c412164d07d904d3adfd13adc
Chun <caochun@gmail.com>
2022-07-30 15:52:56 +0800

    add dependencies for storages

---
+++ src/main/java/info/nemoworks/mmos/storage/h2/EntityDB.java
@@ -0,0 +1,21 @@
+package info.nemoworks.mmos.storage.h2;
+
+import info.nemoworks.mmos.model.Entity;
+import info.nemoworks.mmos.storage.EntityStorage;
+
+public class EntityDB implements EntityStorage {
+    @Override
+    public Entity get(String id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Entity entity) {
+
+    }
+
+    @Override
+    public Entity save(Entity entity) {
+        return null;
+    }
+}
